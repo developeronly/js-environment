1ba9372cefdd33bbb5efdfbb7e012e66096f0c41
developeronly <developeronly@gmail.com>
2017-08-13 00:36:37 +0530

    Configured webpack as a bundler.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import numeral from 'numeral'; //Handy library for formatting numbers.
+import './index.css';
+
+const courseValue = numeral(1000).format('$0,0.00');
+console.log(`This is how much might cost ${courseValue}`);
