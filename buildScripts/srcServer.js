ddc0e117475dd59acd4bb03a342e7a72cc5b54e7
developeronly <developeronly@gmail.com>
2017-08-13 16:08:02 +0530

    Centralized API calls. Mocking HTTP calls.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -22,6 +22,16 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html')); //dirname that will get us the directory name that we're currently running in.
 });
 
+//Example API
+app.get('/users', function(req, res) {
+  // Hard coding for now
+  res.json([
+    {"id": 1,"firstName":"Dheeraj","lastName":"Karande","email":"dheeraj@gmail.com"},
+    {"id": 2,"firstName":"Shivraj","lastName":"Patil","email":"shivraj@yahoo.com"},
+    {"id": 3,"firstName":"Sarang","lastName":"Ingole","email":"sarang@hotmail.com"}
+  ]);
+});
+
 //Error handling
 app.listen(port, function(err) {
   if(err) {
